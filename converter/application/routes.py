382aa0f35d5f3fc3b49d48a2b431636ed9d43ce4
nathanforester <nfeugene86@gmail.com>
2023-06-06 12:28:58 +0100

    first commit

---
+++ converter/application/routes.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, Response, message_flashed 
+import sys
+from flask import jsonify
+from flask import jsonify
+import json
+from flask import json
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/date/<convertedCurrency>', methods=['GET', 'POST'])
+def convertedCurrency(convertedCurrency):
+    try:
+        convertedCurrency = int(float(convertedCurrency))
+    except ValueError:
+        return "ValueError: enter a number"
+    if convertedCurrency < 0:
+        return 'Please enter at least 1'
+    else:
+        convertedCurrency = convertedCurrency * 1.25
+    return str(convertedCurrency)
+    
---
+++ main/application/routes.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from application import app
+import requests
+from flask import render_template, request, redirect, url_for
+import sys
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def date():
+    formData = request.form.get('result')
+    formData = str(formData)
+    convertedCurrency = requests.post('http://converter:5001/result/' + formData)
+    return render_template('convert.html', formData=formData, birthDate=convertedCurrency.text)
+
