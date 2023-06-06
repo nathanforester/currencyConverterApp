282d13bcb446c4545bb32d1b7ff790ce460b568c
nathanforester <nfeugene86@gmail.com>
2023-06-06 14:35:13 +0100

    should be working

--- converter/application/__init__.py
+++ converter/application/__init__.py
@@ -1,7 +1,23 @@
-  
-from flask import Flask
-
+from flask import Flask, jsonify, Response, message_flashed 
+import sys
+from flask import jsonify
+from flask import jsonify
+import json
+from flask import json
+from datetime import datetime
 
 app = Flask(__name__)
 
-from application import routes
+
+@app.route('/result/<converted>', methods=['GET', 'POST'])
+def convertedCurrency(converted):
+    try:
+        converted = int(float(converted))
+    except ValueError:
+        return "ValueError: enter a number"
+    if convertedCurrency < 0:
+        return 'Please enter at least 1'
+    else:
+        converted = converted * 1.25
+    return str(converted)
+    
